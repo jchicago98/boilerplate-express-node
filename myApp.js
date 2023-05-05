1bb23cb11114c56c2ddbfa8c3708e5cc5cfdec9e
Jorge Morin <jmorin1@hawk.iit.edu>
2023-05-05 15:05:33 -0500

    Finished 67% of problems from free code camp

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
@@ -9,19 +11,48 @@ let app = express();
 
 absolutePath = __dirname + '/views/index.html';
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(absolutePath);
 })
 
 // absolutePath = __dirname + '/public';
 
-app.use("/public", express.static(__dirname + '/public'));
+// app.use("/public", express.static(__dirname + '/public'));
+
+// app.get("/json", (req, res) => {
+//     res.json({
+//         message: "Hello json"
+//     });
+// });
 
 
+// app.get("/json", (req, res) => {
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         //response = "Hello World".toUpperCase();
+//         res.json({
+//             message: "Hello json".toUpperCase()
+//         });
+//     } else {
+//         res.json({
+//             message: "Hello json"
+//         });
+//     }
+// })
 
+// app.use(function(req, res, next) {
+//     var string = req.method + " " + req.path + " - " + req.ip;
+//     console.log(string);
+//     next();
+// });
 
-
-
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.send({
+        time: req.time
+    });
+});
 
 
 
